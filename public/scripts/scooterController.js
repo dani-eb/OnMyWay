b149f499e9ec7d51e55213ff34a0fad48948fd24
LazerPlatypus(Laptop) <aholt@student.neumont.edu>
2019-11-25 19:55:30 -0700

    Update scooterController.js

--- public/scripts/scooterController.js
+++ public/scripts/scooterController.js
@@ -5,32 +5,40 @@ scooter_button.addEventListener('click', (evt) =>{
     
     const request_headers = new Headers();
     request_headers.append('Content-Type', 'application/json');
-
-    const request_body = {
-        "query":"query ($lat: Float!, $lng: Float!, $typeVehicles: [VehicleType]) {\n  vehicles (lat: $lat, lng: $lng, typeVehicles: $typeVehicles) {\n    id\n    type\n    lat\n    lng\n    provider {\n      name\n    }\n  }\n}",
-        "variables": {
-            "lat": 40.766043,
-            "lng": -111.890814,
-            "typeVehicles": [
-                "SCOOTER"
-            ]
-        }
-        
-        // "query":"query ($lat: Float!, $lng: Float!) {\n  vehicles(lat: $lat, lng: $lng) {\n\t\tid\n  }\n}","variables":{"lat":48.856614,"lng":2.352222}}
-        // query:"($lat: Float!, $lng: Float!, $typeVehicles: [VehicleType]) {\n vehicles (lat: $lat, lng: $lng, typeVehicles: $typeVehicles) {\n\t\tid\ntype\nlat\nlng\nprovider {\nname\n}\n}\n}",
-        // variables:{"lat": 40.766043,"lng": -111.890814,"typeVehicles": ["SCOOTER"]}
-    };
-
-    const request_settings = {
+    request_headers.append('Host', 'api.multicycles.org');
+    request_headers.append('Accept', '*/*');
+    
+    console.log("fetching here");
+    fetch('https://api.multicycles.org/v1?access_token=9zrG3cRPdgKwJiOeDh1BmZ9Kly2Ul5hM', {
         method: 'POST',
-        headers: request_headers,
-        mode: 'no-cors', // change cors if errors
-        body: request_body
-    };
-
-    const request = fetch(url, request_settings).then((response) => {
-        console.log(response);
-    });
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: `query ($lat: Float!, $lng: Float!, $typeVehicles: [VehicleType]) {
+                vehicles (lat: $lat, lng: $lng, typeVehicles: $typeVehicles) {
+                    id
+                    type
+                    lat
+                    lng
+                    provider {
+                        name
+                    }
+                }
+            }`,
+            variables: {
+                lat: 40.766043,
+		        lng: -111.890814,
+		        typeVehicles: [
+    		        "SCOOTER"
+		        ]
+            }
+        })
+    }).then(response => {
+        return response.json()
+    }).then(responseAsJson => {
+        console.log(responseAsJson.data)
+    })
 
 });
 
