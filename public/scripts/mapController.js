15bf8998a64bc7b3aaae507d5bf54e330e16ccc2
VegaNan <47089431+VegaNan@users.noreply.github.com>
2019-11-25 19:45:13 -0700

    Update mapController.js

--- public/scripts/mapController.js
+++ public/scripts/mapController.js
@@ -14,6 +14,7 @@ var map = tomtom.L.map('map', {
 
 //This is an array of the scooters
 var ScooterPoints;
+var ScooterMarkerArray = [];
 
 //Zoom in and out with buttons on the top right
 map.zoomControl.setPosition('topright');
@@ -61,7 +62,7 @@ var endLocation = tomtom.searchBox({
 var locationsList = [];
 
 // Update the searchbox widget content when the markers are dragged
-routeOnMapView.on(routeOnMapView.Events.MarkerDragEnd, function(result) {
+routeOnMapView.on(routeOnMapView.Events.MarkerDragEnd, function (result) {
     //if the index is 0, it is the start, else it is the end
     var searchBox = result.markerIndex === 0 ? startLocation : endLocation;
     //the searchBox data becomes the result
@@ -81,64 +82,83 @@ function updateRouteEnd(pos) {
     updateMarkers();
 }
 
-startLocation.on(startLocation.Events.ResultClicked, function(event) {
+startLocation.on(startLocation.Events.ResultClicked, function (event) {
     updateRouteStart(event.data.position);
     window.start_location = event.data.position;
 });
 
-endLocation.on(endLocation.Events.ResultClicked, function(event) {
+endLocation.on(endLocation.Events.ResultClicked, function (event) {
     updateRouteEnd(event.data.position);
     window.end_location = event.data.position;
 });
 
-startLocation.on(startLocation.Events.ClearButtonClicked, function() {
+startLocation.on(startLocation.Events.ClearButtonClicked, function () {
     updateRouteStart();
     window.startLocation = undefined;
 });
 
-endLocation.on(endLocation.Events.ClearButtonClicked, function() {
+endLocation.on(endLocation.Events.ClearButtonClicked, function () {
     updateRouteEnd();
     window.endLocation = undefined;
 });
 
+//this one runs when the map inits
 updateMarkers();
 
 var markers = tomtom.L.markerClusterGroup();
 
-function getUpdatedScooterPoints(){
-    //Get the scooter points here
-    ScooterPoints = [
-        [1, -33.87, 181.21, 'Sydneyyyyy'],
-        [2, -37.81, 181.96, 'Melbourne'],
-        [3, -27.46, 181.02, 'Brisbane']
-    ];
-    // ScooterPoints = [
-    //     [1, -33.87, 191.21, 'Sydneyyyyy'],
-    //     [2, -37.81, 191.96, 'Melbourne'],
-    //     [3, -27.46, 191.02, 'Brisbane']
-    // ];
+//testing stuff
+var i = 1;
+function getUpdatedScooterPoints() {
+    //gimme the points
+    if(i === 1){
+        ScooterPoints = [
+            [1, -33.87, 161.21, 'Sydneyyyyy'],
+            [2, -37.81, 161.96, 'Melbourne'],
+            [3, -27.46, 161.02, 'Brisbane']
+        ];
+    }else if(i === 2){
+        ScooterPoints = [
+            [1, -33.87, 181.21, 'Sydneyyyyy'],
+            [2, -37.81, 181.96, 'Melbourne'],
+            [3, -27.46, 181.02, 'Brisbane']
+        ];
+    }else{
+        ScooterPoints = [];
+    }
+}
+
+function createScooterMarkers() {
+    ScooterMarkerArray = new Array();
+    if(typeof ScooterPoints !== 'undefined' && ScooterPoints.length){
+        ScooterPoints.forEach(function (point) {
+            console.log("in for each for " + point[3]);
+            var title = point[3],
+                marker = tomtom.L.marker(new tomtom.L.LatLng(point[1], point[2]), { title: title });
+            marker.bindPopup(title);
+            ScooterMarkerArray.push(marker);
+        });
+    }
 }
 
 async function updateMarkers() {
     getUpdatedScooterPoints();
+    createScooterMarkers();
     if (markers) {
         console.log("in remove layer");
-        if (typeof markers !== 'undefined') {
-            console.log("in remove markers");
-
+        if (typeof markers !== 'undefined' && markers.length) {
+            //check if updated and remove
+            ScooterMarkerArray.forEach(function (marker) {
+                marker.update();
+            });
             markers.removeLayers();
         }
         await map.removeLayer(markers);
     }
-
+    //testing var
+    i = i + 1;
     markers = tomtom.L.markerClusterGroup();
-    console.log("in marker creation");
-
-    ScooterPoints.forEach(function(point) {
-        console.log("in for each for " + point[3]);
-        var title = point[3],
-            marker = tomtom.L.marker(new tomtom.L.LatLng(point[1], point[2]), { title: title });
-        marker.bindPopup(title);
+    ScooterMarkerArray.forEach(function(marker) {
         markers.addLayer(marker);
     });
 
