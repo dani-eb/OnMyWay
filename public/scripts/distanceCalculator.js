fde23602d451cb7e685585eaabcb51cde98a1098
LazerPlatypus(Laptop) <aholt@student.neumont.edu>
2019-12-02 18:12:28 -0700

    fixed distanceCalculator

--- public/scripts/distanceCalculator.js
+++ public/scripts/distanceCalculator.js
@@ -5,7 +5,7 @@ const calculateDistanceBetweenCoordinates = (startLat, startLon, endLat, endLon)
     var startLatRadians = startLat.toRadians(); // start lat
     var endLatRadians = endLat.toRadians(); // end
     var differenceLatRadians = (endLat-startLat).toRadians();
-    var differenceLonRadians = (endLon-startLan).toRadians();
+    var differenceLonRadians = (endLon-startLon).toRadians();
     
     var a = Math.pow(Math.sin(differenceLatRadians/2),2)+Math.cos(startLatRadians)*Math.cos(endLatRadians) * Math.pow(Math.sin(differenceLonRadians/2),2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
